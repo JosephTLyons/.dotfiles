8af70c0d3b8c034c97f1a625b2ad550bd89b5a96
Joseph T Lyons <JosephTLyons@users.noreply.github.com>
2019-12-14 16:59:51 -0500

    More tidying up

--- make_symlinks.py
+++ make_symlinks.py
@@ -12,11 +12,11 @@ def make_symlink(action, path_to_file, path_to_symlink):
 
 def force_make_symlink(path_to_file, path_to_symlink):
     try:
-        make_symlink("created", path_to_symlink, path_to_file)
+        make_symlink("created", path_to_file, path_to_symlink)
     except OSError as e:
         if e.errno == errno.EEXIST:
             remove(path_to_symlink)
-            make_symlink("overwritten", path_to_symlink, path_to_file)
+            make_symlink("overwritten", path_to_file, path_to_symlink)
 
 def make_all_symlinks():
     excluded_items = [
@@ -26,19 +26,19 @@ def make_all_symlinks():
         "make_symlinks.py"
     ]
 
-    home_path = Path.home()
-    dotfile_directory_path = Path(home_path, ".dotfiles")
+    home_directory_path = Path.home()
+    dotfile_directory_path = Path(home_directory_path, ".dotfiles")
 
     files = [file for file in listdir(dotfile_directory_path) if isfile(join(dotfile_directory_path, file)) and str(file) not in excluded_items]
 
     for file in files:
         absolute_path_to_dotfile = Path(dotfile_directory_path, file)
-        absolute_path_to_symlink = Path(home_path, file)
+        absolute_path_to_symlink = Path(home_directory_path, file)
 
         force_make_symlink(absolute_path_to_dotfile, absolute_path_to_symlink)
 
     # Additional symlinks generated here
-    force_make_symlink(Path(dotfile_directory_path, ".zprofile"), Path(home_path, ".profile"))
+    force_make_symlink(Path(dotfile_directory_path, ".zprofile"), Path(home_directory_path, ".profile"))
 
 if __name__ == "__main__":
     make_all_symlinks()
