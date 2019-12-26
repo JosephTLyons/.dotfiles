1e89d89083a51f88bb4a58ee0860b69e85162442
Joseph T Lyons <JosephTLyons@users.noreply.github.com>
2019-12-25 20:14:29 -0500

    Symlinks can now be deleted

--- create_symlinks.py
+++ create_symlinks.py
@@ -11,14 +11,13 @@ def create_symlink(action, path_to_file, path_to_symlink):
     print("Symlink", action + ":", path_to_symlink, "->", path_to_file)
 
 def force_create_symlink(path_to_file, path_to_symlink):
-    try:
-        create_symlink("created", path_to_file, path_to_symlink)
-    except OSError as error:
-        if error.errno == errno.EEXIST:
-            os.remove(path_to_symlink)
-            create_symlink("overwritten", path_to_file, path_to_symlink)
+    if not os.path.isfile(path_to_symlink):
+        create_symlink("Created", path_to_file, path_to_symlink)
+    else:
+        os.remove(path_to_symlink)
+        create_symlink("Overwritten", path_to_file, path_to_symlink)
 
-def create_symlinks():
+def create_symlink_to_dotfile_dictionary():
     excluded_items = [
         ".DS_Store"
     ]
@@ -28,23 +27,44 @@ def create_symlinks():
     home_directory_path = pathlib.Path.home()
     dotfile_directory_path = pathlib.Path(os.getcwd() + "/files")
 
+    symlink_to_dotfile_dictionary = {}
+
     for file in os.listdir(dotfile_directory_path):
         if os.path.isfile(os.path.join(dotfile_directory_path, file)) and file not in excluded_items:
-            absolute_path_to_dotfile = pathlib.Path(dotfile_directory_path, file)
             absolute_path_to_symlink = pathlib.Path(home_directory_path, file)
+            absolute_path_to_dotfile = pathlib.Path(dotfile_directory_path, file)
+            symlink_to_dotfile_dictionary[absolute_path_to_symlink] = absolute_path_to_dotfile
 
-            force_create_symlink(absolute_path_to_dotfile, absolute_path_to_symlink)
+    # Additional symlinks added here
+    symlink_to_dotfile_dictionary[pathlib.Path(home_directory_path, ".profile")] = pathlib.Path(dotfile_directory_path, ".zprofile")
 
-    # Additional symlinks generated here
-    force_create_symlink(
-        pathlib.Path(dotfile_directory_path, ".zprofile"),
-        pathlib.Path(home_directory_path, ".profile")
-    )
+    return symlink_to_dotfile_dictionary
+
+def create_symlinks():
+    for (absolute_path_to_symlink, absolute_path_to_dotfile) in create_symlink_to_dotfile_dictionary().items():
+        force_create_symlink(absolute_path_to_dotfile, absolute_path_to_symlink)
+
+def delete_symlinks():
+    for absolute_path_to_symlink in create_symlink_to_dotfile_dictionary().keys():
+        if os.path.isfile(absolute_path_to_symlink):
+            os.remove(absolute_path_to_symlink)
+            print("Symlink Deleted:", absolute_path_to_symlink)
+        else:
+            print("Symlink Does Not Exist:", absolute_path_to_symlink)
 
 if __name__ == "__main__":
-    create_symlinks()
+    print("1: Create Symlinks")
+    print("2: Delete Symlinks")
 
+    user_input = int(input())
 
+    if user_input == 1:
+        create_symlinks()
+    elif user_input == 2:
+        delete_symlinks()
+    else:
+        print("Invalid Input")
 
-
-# Have main run a while loop asking if user wants to create or delete symlinks
+# Rename script to something that captures both creation and deletion
+# Clean up long lines
+# Try except on non-integer input?
