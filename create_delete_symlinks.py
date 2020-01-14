5c39bc799113afcf83ccf85076af0af185b41d38
Joseph T Lyons <JosephTLyons@gmail.com>
2020-01-13 22:39:43 -0500

    Made file more "symmetrical" it its execution of deletion and creation of symlinks, other general output cleanup

--- create_delete_symlinks.py
+++ create_delete_symlinks.py
@@ -4,24 +4,23 @@ import glob
 import os
 import pathlib
 
-
-def create_symlink(action, path_to_file, path_to_symlink):
-    os.symlink(path_to_file, path_to_symlink)
-    print("Symlink", action + ":", path_to_symlink, "->", path_to_file)
-
-def force_create_symlink(path_to_file, path_to_symlink):
+def create_symlink(path_to_file, path_to_symlink):
     try:
-        create_symlink("Created", path_to_file, path_to_symlink)
+        os.symlink(path_to_file, path_to_symlink)
+        action = "Created"
     except FileExistsError:
-        os.remove(path_to_symlink)
-        create_symlink("Overwritten", path_to_file, path_to_symlink)
+        action = "Already Exists"
+
+    print("Symlink", action + ":", path_to_symlink, "->", path_to_file)
 
 def delete_symlink(absolute_path_to_symlink):
     try:
         os.remove(absolute_path_to_symlink)
-        print("Symlink Deleted:", absolute_path_to_symlink)
+        action = "Deleted"
     except FileNotFoundError:
-        print("Symlink Does Not Exist:", absolute_path_to_symlink)
+        action = "Does Not Exist"
+
+    print("Symlink", action + ":", absolute_path_to_symlink)
 
 def add_default_items_to_symlink_to_dotfile_dictionary(
     symlink_to_dotfile_dictionary,
@@ -65,11 +64,13 @@ def create_symlink_to_dotfile_dictionary():
     return symlink_to_dotfile_dictionary
 
 def create_symlinks():
-    for (absolute_path_to_symlink, absolute_path_to_dotfile) in create_symlink_to_dotfile_dictionary().items():
-        force_create_symlink(absolute_path_to_dotfile, absolute_path_to_symlink)
+    for (i, (absolute_path_to_symlink, absolute_path_to_dotfile)) in enumerate(create_symlink_to_dotfile_dictionary().items()):
+        print("{}) ".format(i + 1), end="")
+        create_symlink(absolute_path_to_dotfile, absolute_path_to_symlink)
 
 def delete_symlinks():
-    for absolute_path_to_symlink in create_symlink_to_dotfile_dictionary().keys():
+    for (i, absolute_path_to_symlink) in enumerate(create_symlink_to_dotfile_dictionary().keys()):
+        print("{}) ".format(i + 1), end="")
         delete_symlink(absolute_path_to_symlink)
 
 if __name__ == "__main__":
@@ -80,6 +81,8 @@ if __name__ == "__main__":
         user_input = int(input())
     except ValueError:
         user_input = 0
+
+    print()
 
     if user_input == 1:
         create_symlinks()
